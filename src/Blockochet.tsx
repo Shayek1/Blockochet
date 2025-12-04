692132bc5903da9da5c50658d18c509e3d800d1f
Shayek Ahmed <shayek.a@outlook.com>
2025-12-04 20:42:13 +0000

    update wall collision comments

--- src/Blockochet.tsx
+++ src/Blockochet.tsx
@@ -32,12 +32,12 @@ const canvasReference = useRef<HTMLCanvasElement | null>(null)
             ball.y += ball.dy;
 
             //wall collision
-            //Horizontal
+            //Left and right walls
             if (ball.x-ball.radius < 0 || ball.x + ball.radius > canvas.width){
                 ball.dx *= -1;
             }
 
-            //Vertical - only collision on the ceiling as the balls  bottom
+            //top wall - only collision on the ceiling as the balls  bottom
             if (ball.y - ball.radius < 0){
                 ball.dy *= -1;
             }
