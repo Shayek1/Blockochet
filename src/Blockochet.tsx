8ef739962da1ad3cbc19e1293b69831cf02b8b75
Shayek Ahmed <shayek.a@outlook.com>
2025-12-04 20:05:22 +0000

    created Ball

--- src/Blockochet.tsx
+++ src/Blockochet.tsx
@@ -10,12 +10,23 @@ const canvasReference = useRef<HTMLCanvasElement | null>(null)
         const ccontext = canvas.getContext("2d");
         if(!ccontext) return;
 
+        //creating the ball
+        const ball = {
+            color: "white",
+            x: canvas.height / 2,
+            y: canvas.width / 2,
+            radius: 10
+        }
+
         let animationFrameId: number;
 
         const loop = () => {
             ccontext.clearRect(0, 0, canvas.width, canvas.height);
-            ccontext.fillStyle = "purple";
-            ccontext.fillRect(10,10,30,30);
+
+            ccontext.fillStyle = ball.color; //will fill the ball as white
+            ccontext.beginPath();
+            ccontext.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+            ccontext.fill();
 
             animationFrameId = requestAnimationFrame(loop);
         };
