3204337ee8584eee2e1a46ee56da146e13e86e26
Shayek Ahmed <shayek.a@outlook.com>
2025-12-06 20:43:50 +0000

    create constants for brick configurations

---
+++ src/game/brickConfig.ts
@@ -0,0 +1,11 @@
+
+export const brickRows = 5;
+export const brickCols = 8;
+
+export const brickWidth = 10;
+export const brickHeight = 5;
+export const brickPadding = 5;
+
+export const brickOffsetTop = 40;
+export const brickOffsetLeft = 30;
+
