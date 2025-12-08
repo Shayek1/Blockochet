9fc8da7922e17dd020602f53b5eac87e5e73baa2
Shayek Ahmed <shayek.a@outlook.com>
2025-12-08 00:15:40 +0000

    create Brick interface

--- src/game/brickConfig.ts
+++ src/game/brickConfig.ts
@@ -9,3 +9,11 @@ export const brickPadding = 5;
 export const brickOffsetTop = 40;
 export const brickOffsetLeft = 30;
 
+export interface Brick{
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    status: number; //either a 1 for alive or 0 for destroyed
+}
+
